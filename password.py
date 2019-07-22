99e6ccc43df5f2c4bcae51c021aebfcd65bff112
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-22 10:18:36 +0300

    added the three files

---
+++ password.py
@@ -0,0 +1,39 @@
+class Password:
+
+    my_list = []
+
+    def __init__(self, userName, password):
+        self.userName = userName
+        self.password = password
+
+    def savePassword(self):
+        Password.my_list.append(self)
+
+    def deletePassword(self):
+        Password.my_list.remove(self)
+
+    @classmethod
+    def display_passwords(cls):
+        return cls.my_list
+
+
+class User:
+    user_list = []
+
+    def __init__(self, name, Password):
+        self.name = name
+        self.Password = Password
+
+    def saveUser(self):
+        User.user_list.append(self)
+
+    @classmethod
+    def check_user(cls,name,Password):
+        current_user = ''
+        for user in User.user_list:
+            if (user.name == name and user.Password == Password):
+                current_user = user.name
+                return current_user
+
+
+
