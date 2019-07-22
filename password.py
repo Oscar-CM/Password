5c2d8afa743fc512b59cd151a143b133fef0ea5e
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-22 10:21:33 +0300

    added the user class and initialized

--- password.py
+++ password.py
@@ -5,3 +5,12 @@ class Password:
     def __init__(self, userName, password):
         self.userName = userName
         self.password = password
+
+
+
+class User:
+    user_list = []
+
+    def __init__(self, name, Password):
+        self.name = name
+        self.Password = Password
