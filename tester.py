d476fbf14e5fefe5d6f1f647f9d35140441ac618
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-22 10:25:08 +0300

    tested that the two classes had been created properly

--- tester.py
+++ tester.py
@@ -5,3 +5,14 @@ class Testing (unittest.TestCase):
     def setUp(self):
         self.new_person = Password("Oscar", "1234")
         self.new_user = User ("Cheru", "123")
+
+    def test_init(self):
+        '''
+        Testing both of by classes initializations
+        '''
+        self.assertEqual(self.new_person.userName,"Oscar")
+        self.assertEqual(self.new_person.password,"1234")
+        self.assertEqual(self.new_user.name, "Cheru")
+        self.assertEqual(self.new_user.Password, "123")
+
+        ''' Testing the password class'''
