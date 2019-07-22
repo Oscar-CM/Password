a78897436f8219637dc6441dde2c12930b168481
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-22 10:27:14 +0300

    added the delete password and the funtionality to display all the credentials

--- password.py
+++ password.py
@@ -5,9 +5,16 @@ class Password:
     def __init__(self, userName, password):
         self.userName = userName
         self.password = password
-        
+
     def savePassword(self):
         Password.my_list.append(self)
+    
+    def deletePassword(self):
+        Password.my_list.remove(self)
+
+    @classmethod
+    def display_passwords(cls):
+        return cls.my_list
 
 
 
